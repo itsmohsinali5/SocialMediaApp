0a9ea30f3037852c64908fdb6eb4f7a6a751e820
Osama Naeem <15-EE-149@students.Uettaxila.edu.pk>
2022-03-15 17:46:52 +0500

    singleChannel changes

--- src/components/SingleChannel/SingleChannelNav.js
+++ src/components/SingleChannel/SingleChannelNav.js
@@ -11,8 +11,48 @@ import { VerifiedTooltip } from "../Atomics/CustomCheckTooltips/CustomCheckToolt
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function SingleChannelNav() {
+  let history = useHistory();
+
+  const [colorVideos, setColorVideos] = useState(true);
+  const [colorPlaylist, setColorPlaylist] = useState(false);
+  const [colorChannel, setColorChannel] = useState(false);
+  const [colorAbout, setColorAbout] = useState(false);
+
+  const TurnPlaylistRed = () => {
+    setColorPlaylist(true);
+    setColorVideos(false);
+    setColorChannel(false);
+    setColorAbout(false);
+    // history.push("/playlist")
+  };
+  const TurnVideosRed = () => {
+
+    setColorVideos(true);
+    setColorPlaylist(false);
+    setColorChannel(false);
+    setColorAbout(false);
+ 
+  };
+
+  const TurnChannelRed = () => {
+    setColorChannel(true);
+    setColorPlaylist(false);
+    setColorVideos(false);
+    setColorAbout(false);
+ 
+  };
+
+  const TurnAboutRed = () => {
+    setColorAbout(true);
+    setColorChannel(false);
+    setColorPlaylist(false);
+    setColorVideos(false);
+  };
+
   return (
     <>
       <div className="single-channel-nav">
@@ -25,32 +65,40 @@ export default function SingleChannelNav() {
 
           <Navbar.Collapse id="navbarSupportedContent">
             <Nav as="ul" className="mr-auto">
-              <li className="nav-item active">
+              <li
+                onClick={TurnVideosRed}
+                className={colorVideos ? "nav-item active" : "nav-item"}
+              >
                 <Link className="nav-link " to="#">
                   Videos
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/playlist">
+                <li
+                  onClick={TurnPlaylistRed}
+                  className={colorPlaylist ? "nav-item active" : "nav-item"}
+                  >
+                    {console.log("color",colorPlaylist)}
+              <Link className="nav-link" to="/playlist">
                   Playlists
                 </Link>
-              </li>
-              <li className="nav-item">
+                </li>
+              <li
+                onClick={TurnChannelRed}
+                className={colorChannel ? "nav-item active" : "nav-item"}
+              >
                 <Link className="nav-link">Channels</Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link">Discussion</Link>
-              </li>
-              <li className="nav-item">
+              <li  onClick={TurnAboutRed}
+                className={colorAbout ? "nav-item active" : "nav-item"}>
                 <Link className="nav-link">About</Link>
               </li>
 
-              <NavDropdown title="Donate" id="basic-nav-dropdown">
+              {/* <NavDropdown title="Donate" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#">Another action</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#">Something</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
             <Form inline className=" my-2 my-lg-0">
               <FormControl
